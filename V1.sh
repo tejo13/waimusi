0ac6eb8c5439601049d719f37bdf4c684550fa8f
tejo13 <47310646+tejo13@users.noreply.github.com>
2019-02-04 14:22:51 +0900

    Add files via upload

---
+++ V1.sh
@@ -0,0 +1,155 @@
+#!/system/xbin/bash
+#Author InYouMRa27
+#Mau Ngapin Lu nyet!!
+#Ga Usah Recode Goblok!!
+
+
+clear
+
+sleep 1
+
+echo "#####################################"
+toilet NUYUL
+echo "#####################################"
+date
+echo "##################################"
+echo "     [+] CREATE BY MR.A27"
+echo "##################################"
+echo
+echo "[#############################]"
+echo "[#############################]"
+echo $"[1] NUYUL KUBIK NEW"
+echo "[#############################]"
+echo "[#############################]"
+echo $"[2] NUYUL CAPING NEW"
+echo "[#############################]"
+echo "[#############################]"
+echo $"[3] NUYUL CHASTREE NEW"
+echo "[#############################]"
+echo "[#############################]"
+echo $"[4] NUYUL VEEU NEW"
+echo "[#############################]"
+echo "[#############################]"
+echo $"[0] EXIT "
+echo "[#############################]"
+echo "[#############################]"
+read -p "[×╔[Pilih Nomer]
+[×╚══════════=>" gg
+
+
+if [ $gg = 1 ] || [ $gg = 1 ]
+
+
+then
+
+
+clear
+
+toilet KUBIK
+
+pkg install php
+
+pkg install nano
+
+git clone https://github.com/radenvodka/kubik-bot
+
+cd kubik-bot
+
+nano kubik.php
+
+echo "UDAH ke nano? ketik ini php kubik.php"|lolcat
+
+
+fi
+
+
+if [ $gg = 2 ] || [ $gg = 2 ]
+
+
+then
+
+
+clear
+
+toilet CAPING
+
+pkg install php
+
+git clone https://github.com/anggaid14/caping.git
+
+cd caping
+
+php run.php
+
+
+fi
+
+
+if [ $gg = 3 ] || [ $gg = 3 ]
+
+
+then
+
+
+clear
+
+toilet CASHTREE
+
+pkg install nano
+
+git clone https://github.com/radenvodka/cashtree/
+
+cd cashtree
+
+sleep1
+
+nano cashtree.php
+
+echo "UDAH ke nano? ketik ini php cashtree.php"|lolcat
+
+
+fi
+
+
+if [ $gg = 4 ] || [ $gg = 4 ]
+
+
+then
+
+
+clear
+
+toilet VEEU
+
+pkg install nano
+
+git clone https://github.com/skyqnhataa/veeubot
+
+cd veeubot
+
+sleep1
+
+nano veeusky.php
+
+echo "UDAH ke nano? ketik ini php skyveeubot.php"|lolcat
+
+
+fi
+
+
+if [ $gg = 0 ] || [ $gg = 0 ]
+
+
+then
+
+
+toilet  EXIT
+
+echo "[÷] THANKS SUDAH MENGGUNAKAN TOOLS INI.."| lolcat
+
+sleep 1
+
+exit
+
+fi
+
